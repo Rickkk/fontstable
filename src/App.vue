0915de1dc61041e0daa3c11046f5cda6f07838af
Kirill Korobkov <k.korobkov@iml.ru>
2018-12-05 19:27:37 +0300

    проблема с es2015

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,63 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
-  </div>
+        <div id="fonttable" class="container fonttable">
+            <div class="page-header">
+                <div>
+                    <div class="row">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-1">
+                            Размер
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input name="maininput" placeholder="Начните печатать текст" type="text" v-model='maininput'>
+                            </input>
+                        </div>
+                        <div class="col-md-1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 fontelem">
+                  <div class="row fontname pd5">
+                    Verdana
+                  </div>
+                  <div class="row text">
+                    {{maininput}}
+                  </div>
+                  <div class="row">
+                    <div class="col-md-11">
+                      
+                    </div>
+                    <div class="col-md">
+                      <button>+</button>
+                    </div>
+                  </div>
+                </div>      
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4">
+                </div>                                                        
+            </div>
+        </div>
 </template>
+
+<!-- https://itnext.io/@petrefax66 -->
+
+<script>
+let = "abc";
+// import HelloComponent from './components/HelloComponent.vue'
+export default {
+  // components: {
+  //   HelloComponent
+  // }
+  data: function()
+  {
+  	return {
+  		maininput: 'ewr'
+  	}
+  }
+}
+</script>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,7 @@
 'use strict'
+var path = require('path');
 const { VueLoaderPlugin } = require('vue-loader')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: [
@@ -10,10 +12,37 @@ module.exports = {
       {
         test: /\.vue$/,
         use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
       }
+    }            
     ]
   },
+  devServer: {
+    inline:true,
+    port: 10000,
+    watchContentBase: true,
+     publicPath: '/dist/',
+     contentBase: path.join(__dirname, './'),
+
+  },  
   plugins: [
-    new VueLoaderPlugin()
+   new UglifyJSPlugin(),
+   new VueLoaderPlugin()    
   ]
 }
