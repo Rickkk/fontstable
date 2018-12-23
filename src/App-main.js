0651a1f42dc5bb5720e38f542c079c9ea15a558f
Kirill Korobkov <k.korobkov@iml.ru>
2018-12-23 14:33:42 +0300

    изменена верстка страницы. добавлены основные функции

--- src/App-main.js
+++ src/App-main.js
@@ -37,9 +37,54 @@ export default {
 
   	let bc = 1;
   	return {
-  		maininput: '',
+  		maininput: 'Какой-то текст',
   		fontAr: fontAr,
-  		fontSize: 14
+  		fontSize: 14,
+      fontFamilyList: '',
+      selectedFonts: []
   	}
-  }
+  },
+  methods:
+  {
+    fontSizeInc: function()
+    {
+      this.fontSize++;
+    },
+    fontSizeDec: function()
+    {
+      if(this.fontSize != 1)
+      this.fontSize--;
+    },
+    addToSelected: function(item)
+    {
+      if(this.selectedFonts.indexOf(item) == -1 )
+      {
+          this.selectedFonts.push(item);
+      }
+    },
+    removeFromSelected: function(item)
+    {
+      let pos = this.selectedFonts.indexOf(item);
+      if(pos > -1)
+      {
+        this.selectedFonts.splice(pos, 1);
+      }
+    }
+    ,
+    isSelected: function(item)
+    {
+      if(item)
+      {
+        return (this.selectedFonts.indexOf(item) != -1 );
+      }else {
+        return false;
+      }
+    }
+  },
+   computed: {
+    selectedList: function()
+    {
+      return this.selectedFonts.join(', ');
+    }
+   }
 }
